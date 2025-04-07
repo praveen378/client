685f85cf2726023a31638d3edd54e8fb41681cfa
praveen378 <115167547+praveen378@users.noreply.github.com>
2025-04-07 19:52:45 +0530

    Update axiousinstance.js

--- src/pages/utility/axiousinstance.js
+++ src/pages/utility/axiousinstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = " http://localhost:3000/api/v1"; // Replace with your API base URL
+const baseUrl = import.meta.env.VITE_BASE_URL; // Replace with your API base URL
 
 export const axiosInstance = axios.create({
   baseURL: baseUrl, // Replace with your API base URL
