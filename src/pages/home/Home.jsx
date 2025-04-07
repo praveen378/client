2d394b63c5880e7a4df0cdd61362d3c10499fd29
praveen378 <115167547+praveen378@users.noreply.github.com>
2025-04-07 21:09:06 +0530

    Update Home.jsx

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -65,10 +65,10 @@ const Home = () => {
   // Setup PeerJS
   useEffect(() => {
     const newPeer = new Peer(undefined, {
-      host: " https://backend-production-0f54.up.railway.app/",
-     port: 443,
-  path: "/peerjs",
-  secure: true,
+      host: "backend-production-0f54.up.railway.app/",
+      port: 443,
+      path: "/peerjs",
+      secure: true,
     });
 
     setPeer(newPeer); // ✅ Set in context
